43766170d04ac418c22bbfe4d3341bd94b1cbbfe
Abraham Davila <branda300@gmail.com>
2022-12-08 10:09:54 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Switch from '@mui/material/Switch';
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 function App() {
   return (
@@ -7,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +20,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
+        <Switch {...label} defaultChecked />
+        <Switch {...label} />
+        <Switch {...label} disabled defaultChecked />
+        <Switch {...label} disabled />
       </header>
     </div>
   );
